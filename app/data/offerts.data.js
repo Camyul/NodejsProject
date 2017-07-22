ba9cd026c03e1335a95edd7fbdb0fc58479bec93
simzzz <swagb3ast@hotmail.com>
2017-07-22 16:00:37 +0300

    Improved offer form validation.

--- app/data/offerts.data.js
+++ app/data/offerts.data.js
@@ -1,5 +1,7 @@
 const BaseData = require('./base/base.data');
 const Offert = require('../../models/offer.model');
+const supportedCountries = ['bulgaria', 'greece'];
+const dateMatcher = new RegExp(/[0-9]{4}-[0-9]{2}-[0-9]{2}/g);
 
 class OffertsData extends BaseData {
     constructor(db) {
@@ -7,8 +9,30 @@ class OffertsData extends BaseData {
     }
 
     _isModelValid(model) {
-        // custom validation 
-        return super._isModelValid(model);
+        if (!(supportedCountries.includes(model.country.toLowerCase()))) {
+            console.log('failed at country validation');
+            return false;
+        }
+        if (model.city.length < 3) {
+            console.log('failed at city validation');
+
+            return false;
+        }
+        if (isNaN(model.price)) {
+            console.log(model.price);
+            return false;
+        }
+        if (isNaN(model.duration) || model.duration < 1) {
+            console.log('failed at duration validation');
+
+            return false;
+        }
+        if (!(model.date.match(dateMatcher))) {
+            console.log('failed at date validation');
+            console.log(model.date);
+            return false;
+        }
+        return true;
     }
 }
 
