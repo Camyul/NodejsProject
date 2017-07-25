df9ea254e8cb8ac10b52a3a8ecd9e378dff3099a
simzzz <swagb3ast@hotmail.com>
2017-07-25 21:08:42 +0300

    Added data validators on forms using toastr that I will set up later

--- static/js/createOffer.js
+++ static/js/createOffer.js
@@ -5,7 +5,16 @@ $(window).load(function() {
 
         event.preventDefault(); // prevent the default submit
 
-        // code goes here
+        const country = $('#country').val();
+        const city = $('#city').val();
+        const price = $('#price').val();
+        const duration = $('#duration').val();
+
+        let countryOk = false;
+        let cityOk = false;
+        let priceOk = false;
+        let durationOk = false
+
 
         $(this).unbind('submit').submit(); // continue the submit unbind preventDefault
     })
--- static/js/sign-up.js
+++ static/js/sign-up.js
@@ -1,12 +1,3 @@
-$(window).load(function() {
-    toastr.info('Toastr and jquery are armed and ready');
-
-    $('#form').submit(function(event) {
-
-        event.preventDefault(); // prevent the default submit
-
-        // code goes here
-
-        $(this).unbind('submit').submit(); // continue the submit unbind preventDefault
-    })
-});
+// $(window).load(function() {
+//     toastr.info('Toastr and jquery are armed and ready');
+// });
