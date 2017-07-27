3fb4e08f5c8326309c424a905bea600e6d1ba238
simzzz <swagb3ast@hotmail.com>
2017-07-27 22:14:59 +0300

    Added member chat page

--- app/routers/users.router.js
+++ app/routers/users.router.js
@@ -7,6 +7,9 @@ const attach = (app, data) => {
     const controller = require('./profileController').init(data);
     const contactController = require('./contactController').init(data);
 
+    const http = require('http').Server(router);
+    const io = require('socket.io')(http);
+
     router
         .get('/', (req, res) => {
             return res.status(200).render('home');
@@ -37,12 +40,22 @@ const attach = (app, data) => {
         .get('/contact', (req, res) => {
             return res.render('contact');
         })
+        .get('/memberchat', (req, res) => {
+            if (!req.isAuthenticated()) {
+                return res.status(401).render('./profile/unauthorized');
+            }
+            return res.status(200).render('./memberChat');
+        })
         .post('/contact', (req, res) => {
             return contactController.submitForm(req, res);
         })
         .post('/contact/subscribe', (req, res) => {
             return contactController.subscribe(req, res);
         });
+
+    io.on('connection', function(socket) {
+        console.log('a user connected');
+    });
     app.use('/', router);
 };
 
