a0343481128bd95ff1177329e182f4af4d4278da
Camyul <vankata.lt@outlook.com>
2017-07-31 18:55:43 +0300

    Correct connectionString Finally

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const port = 80;
-const connectionString = 'mongodb://ec2-54-93-224-213.eu-central-1.compute.amazonaws.com:27017/sharedTravel';
+const connectionString = 'mongodb://ec2-52-59-8-231.eu-central-1.compute.amazonaws.com:27017/sharedTravel';
 
 const sessionSecret = 'Purple Unicorn';
 
